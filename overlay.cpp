f357c03b8852c7bcfc89b518c94f86006b26174b
dpcpointer <heapallocation@proton.me>
2025-06-17 22:14:26 -0600

    Update overlay.cpp
    add adapters

--- overlay.cpp
+++ overlay.cpp
@@ -33,23 +33,23 @@ LRESULT WINAPI window::WndProc(HWND hWnd, UINT Msg, WPARAM wParam, LPARAM lParam
 bool window::create_device() {
     if (!hwnd || width <= 0 || height <= 0)
         return false;
-    
+
     DXGI_SWAP_CHAIN_DESC sd = {};
     sd.BufferCount = 1;
     sd.BufferDesc.Width = width;
     sd.BufferDesc.Height = height;
     sd.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
-    sd.BufferDesc.RefreshRate.Numerator = 60;
+    sd.BufferDesc.RefreshRate.Numerator = 1000;
     sd.BufferDesc.RefreshRate.Denominator = 1;
     sd.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
-    sd.OutputWindow = hwnd;
+    sd.OutputWindow = window::hwnd;
     sd.SampleDesc.Count = 1;
     sd.SampleDesc.Quality = 0;
     sd.Windowed = TRUE;
     sd.SwapEffect = DXGI_SWAP_EFFECT_DISCARD;
     sd.Flags = DXGI_SWAP_CHAIN_FLAG_ALLOW_MODE_SWITCH;
 
-    const D3D_FEATURE_LEVEL featureLevelArray[] = { D3D_FEATURE_LEVEL_11_1, D3D_FEATURE_LEVEL_11_0, D3D_FEATURE_LEVEL_10_1, D3D_FEATURE_LEVEL_10_0 };
+    const D3D_FEATURE_LEVEL featureLevelArray[] = { D3D_FEATURE_LEVEL_11_1, D3D_FEATURE_LEVEL_11_0 };
     UINT create_device_flags = 0;
     D3D_FEATURE_LEVEL feature_level;
 
@@ -57,20 +57,66 @@ bool window::create_device() {
     directx::device = nullptr;
     directx::context = nullptr;
 
-    HRESULT hr = D3D11CreateDeviceAndSwapChain(
-        nullptr,
-        D3D_DRIVER_TYPE_HARDWARE,
-        nullptr,
-        create_device_flags,
-        featureLevelArray,
-        _countof(featureLevelArray),
-        D3D11_SDK_VERSION,
-        &sd,
-        &directx::swap_chain,
-        &directx::device,
-        &feature_level,
-        &directx::context
-    );
+    IDXGIFactory* dxgi_factory = nullptr;
+    HRESULT hr = CreateDXGIFactory(__uuidof(IDXGIFactory), (void**)&dxgi_factory);
+    if (FAILED(hr))
+        return false;
+
+    IDXGIAdapter* selected_adapter = nullptr;
+    SIZE_T max_dedicated_video_memory = 0;
+    IDXGIAdapter* adapter = nullptr;
+    UINT i = 0;
+
+    while (dxgi_factory->EnumAdapters(i, &adapter) != DXGI_ERROR_NOT_FOUND) {
+        DXGI_ADAPTER_DESC adapter_desc;
+        adapter->GetDesc(&adapter_desc);
+        if (adapter_desc.DedicatedVideoMemory > max_dedicated_video_memory) {
+            max_dedicated_video_memory = adapter_desc.DedicatedVideoMemory;
+            if (selected_adapter)
+                selected_adapter->Release();
+            selected_adapter = adapter;
+        }
+        else {
+            adapter->Release();
+        }
+        ++i;
+    }
+
+    if (selected_adapter) {
+        hr = D3D11CreateDeviceAndSwapChain(
+            selected_adapter,
+            D3D_DRIVER_TYPE_UNKNOWN,
+            nullptr,
+            create_device_flags,
+            featureLevelArray,
+            _countof(featureLevelArray),
+            D3D11_SDK_VERSION,
+            &sd,
+            &directx::swap_chain,
+            &directx::device,
+            &feature_level,
+            &directx::context
+        );
+        selected_adapter->Release();
+    }
+    else {
+        hr = D3D11CreateDeviceAndSwapChain(
+            nullptr,
+            D3D_DRIVER_TYPE_HARDWARE,
+            nullptr,
+            create_device_flags,
+            featureLevelArray,
+            _countof(featureLevelArray),
+            D3D11_SDK_VERSION,
+            &sd,
+            &directx::swap_chain,
+            &directx::device,
+            &feature_level,
+            &directx::context
+        );
+    }
+
+    dxgi_factory->Release();
 
     if (FAILED(hr)) {
         hr = D3D11CreateDeviceAndSwapChain(
@@ -87,30 +133,26 @@ bool window::create_device() {
             &feature_level,
             &directx::context
         );
-
         if (FAILED(hr))
             return false;
     }
 
     if (!directx::swap_chain || !directx::device || !directx::context)
         return false;
-    
 
     ID3D11Texture2D* back_buffer = nullptr;
     hr = directx::swap_chain->GetBuffer(0, IID_PPV_ARGS(&back_buffer));
     if (FAILED(hr) || !back_buffer)
         return false;
-    
 
     hr = directx::device->CreateRenderTargetView(back_buffer, nullptr, &directx::render_target_view);
     back_buffer->Release();
     back_buffer = nullptr;
     if (FAILED(hr) || !directx::render_target_view)
         return false;
-    
 
     directx::context->OMSetRenderTargets(1, &directx::render_target_view, nullptr);
-    
+
     D3D11_VIEWPORT viewport = {};
     viewport.Width = static_cast<float>(width);
     viewport.Height = static_cast<float>(height);
@@ -124,6 +166,7 @@ bool window::create_device() {
     ImGui::CreateContext();
     ImGuiIO& io = ImGui::GetIO();
     io.IniFilename = nullptr;
+    ImGui::StyleColorsDark();
     ImGui_ImplWin32_Init(hwnd);
     ImGui_ImplDX11_Init(directx::device, directx::context);
 
@@ -143,7 +186,7 @@ void window::draw() {
     directx::context->OMSetRenderTargets(1, &directx::render_target_view, nullptr);
     directx::context->ClearRenderTargetView(directx::render_target_view, clearColor);
     ImGui_ImplDX11_RenderDrawData(ImGui::GetDrawData());
-    directx::swap_chain->Present(1, 0); 
+    directx::swap_chain->Present(0, 0); 
 }
 
 void window::cleanup() {
@@ -175,7 +218,7 @@ bool window::create_window() {
         randomString[length] = L'\0';
         return randomString;
         };
-
+    
     auto window_name = random_string(16);
     
     WNDCLASSEX wc = {};
@@ -186,13 +229,13 @@ bool window::create_window() {
     wc.lpszClassName = window_name;
     wc.hCursor = LoadCursor(nullptr, IDC_ARROW); 
     wc.hbrBackground = nullptr; 
-
+    
     if (!RegisterClassEx(&wc))
         return false;
     
     window::width = GetSystemMetrics(SM_CXSCREEN);
     window::height = GetSystemMetrics(SM_CYSCREEN);
-
+    
     window::hwnd = CreateWindowEx(
         WS_EX_TOPMOST | WS_EX_TRANSPARENT | WS_EX_TOOLWINDOW,
         window_name,
@@ -203,7 +246,7 @@ bool window::create_window() {
         wc.hInstance,
         nullptr
     );
-
+    
     if (!window::hwnd) {
         UnregisterClass(window_name, wc.hInstance);
         return false;
@@ -212,8 +255,8 @@ bool window::create_window() {
     MARGINS margin = { -1, -1, -1, -1 };
     HRESULT hr = DwmExtendFrameIntoClientArea(window::hwnd, &margin);
     if (FAILED(hr)) {
-        DestroyWindow(window::hwnd);
         UnregisterClass(window_name, wc.hInstance);
+        DestroyWindow(window::hwnd);
         window::hwnd = nullptr;
         return false;
     }
